22d135c769e7cbe361cd5806d7a95982f2e5a4d1
caiomota <caio.mota@manifestohut.com.br>
2019-08-03 19:15:53 -0300

    Added naughtyattributes and dotween, and configured lwrp

---
+++ Assets/Project/Scripts/GMTKJ/TowerDefense/TowerDefenseGameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDefenseGameManager : MonoBehaviour
+{
+    public TowerDefenseGameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Destroy(gameObject);
+
+        Instance = this;
+    }
+
+}
