51ec6c00173d08d3dca913ec2d1eed06254e3ea9
caiomota <caio.mota@manifestohut.com.br>
2019-08-03 18:39:13 -0300

    Added navmesh and simple pathing

---
+++ Assets/Project/Scripts/GMTKJ/Ai/GoToTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace GMTKJ.Ai
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class GoToTarget : MonoBehaviour
+    {
+        private NavMeshAgent navMeshAgent;
+        [SerializeField]
+        private Transform target;
+
+        void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            navMeshAgent.destination = target.position;
+        }
+    }
+}
