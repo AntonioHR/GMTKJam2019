d278db4f5ba9457e9398705220fba892e639f004
caiomota <caio.mota@manifestohut.com.br>
2019-08-04 17:44:44 -0300

    Multiple tweaks

--- Assets/Project/Scripts/GMTKJ/Bullets/Obstacle.cs
+++ Assets/Project/Scripts/GMTKJ/Bullets/Obstacle.cs
@@ -4,6 +4,16 @@ namespace GMTKJ.Bullets
 {
     public class Obstacle : MonoBehaviour, IHittable
     {
+        public BoxCollider[] cols;
+
+        [NaughtyAttributes.Button]
+        public void Safadeza()
+        {
+            foreach(var col in cols)
+            {
+                col.size -= Vector3.one *  .2f;
+            }
+        }
         public void OnHitBy(Bullet bullet)
         {
             AudioManager.Instance.PlayFx("WallDamaged");
