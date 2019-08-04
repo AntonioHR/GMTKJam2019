26761e2fc2dffc5627de521bea39168eda7f89de
Pedro Costa <woodecker@gmail.com>
2019-08-03 21:28:48 -0300

    Wave spawner

---
+++ Assets/Project/Scripts/GMTKJ/Ai/WaveManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GMTKJ.Ai
+{
+    public class WaveManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject enemyPrefab;
+        [SerializeField]
+        private Transform target;
+        [SerializeField]
+        private ScriptableObject[] waves;
+
+        void Start()
+        {
+            foreach(ScriptableObject w in waves)
+            {
+                StartCoroutine(NewWave(w as Wave));
+            }
+        }
+
+        IEnumerator NewWave(Wave wave)
+        {
+            foreach (WaveSection ws in wave.waveSections)
+            {
+                StartCoroutine(NewSection(ws));
+                yield return new WaitForSeconds(ws.Size * ws.timeToSpawn + 3f);
+            }
+            yield return new WaitForSeconds(30f);
+        }
+
+        IEnumerator NewSection(WaveSection waveSection)
+        {
+            foreach (GameObject g in waveSection.enemyPrefabs)
+            {
+                AddEnemy(g);
+                yield return new WaitForSeconds(waveSection.timeToSpawn);
+            }   
+        }
+        
+        void AddEnemy(GameObject prefab)
+        {
+            GameObject e = Instantiate(prefab, this.transform);
+            e.GetComponent<GoToTarget>().SetTarget(target);
+        }
+    }
+}
